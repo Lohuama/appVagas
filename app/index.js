562b75be4ad5552097bd40271c0caa0582ec7fdf
Lohuama <limalorruama98@gmail.com>
2023-10-04 23:39:32 -0300

    welcome

--- app/index.js
+++ app/index.js
@@ -9,8 +9,33 @@ const Home = () => {
     const router = useRouter();
 
     return (
-        <SafeAreaView>
-            <Text>Home</Text>
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%'/>
+                    },
+                    headerRight: () => {
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension='100%'/>
+                    },
+                    headerTitle: "",
+                    
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome/>
+
+                </View>
+            </ScrollView>
+            
         </SafeAreaView>
     )
 }
